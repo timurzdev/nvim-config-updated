56685d66f45f93232b33b60e93d8c94bd524a34f
Timur <tmogus@MacBook-Pro-Timur.local>
2025-10-09 15:51:44 +0300

    treesitter fix

--- lua/config/plugins/treesitter.lua
+++ lua/config/plugins/treesitter.lua
@@ -7,12 +7,15 @@ return {
     config = function()
       require('nvim-treesitter').setup()
 
-      vim.api.nvim_create_autocmd('FileType', {
-        pattern = { 'go', 'lua', 'python', 'c', 'vim', 'markdown', 'brief' },
+      -- Enable highlighting for all supported filetypes automatically
+      vim.api.nvim_create_autocmd({'BufEnter', 'BufWinEnter'}, {
+        pattern = { '*.go', '*.lua', '*.py', '*.c', '*.vim', '*.md', '*.brief' },
         callback = function(args)
           local buf = args.buf
           if vim.bo[buf].buftype == '' then
-            pcall(vim.treesitter.start, buf)
+            vim.schedule(function()
+              pcall(vim.treesitter.start, buf)
+            end)
           end
         end,
       })
