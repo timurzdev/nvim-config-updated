4a70652922b9531a0c5494d27e28f606babd6cf1
Timur Zakhoda <tvzakhoda@avito.ru>
2025-12-07 21:42:48 +0300

    updated colorscheme

--- lua/config/lazy.lua
+++ lua/config/lazy.lua
@@ -27,8 +27,34 @@ require("lazy").setup({
     {
       "catppuccin/nvim",
       name = "catppuccin",
+      lazy = false,
       priority = 1000,
       config = function()
+        require("catppuccin").setup({
+          flavour = "mocha",
+          transparent_background = true,
+          term_colors = true,
+          integrations = {
+            cmp = true,
+            dap = true,
+            dap_ui = true,
+            gitsigns = true,
+            mason = true,
+            mini = { enabled = true },
+            native_lsp = {
+              enabled = true,
+              underlines = {
+                errors = { "undercurl" },
+                hints = { "undercurl" },
+                warnings = { "undercurl" },
+                information = { "undercurl" },
+              },
+            },
+            neotest = true,
+            telescope = { enabled = true },
+            treesitter = true,
+          },
+        })
         vim.cmd.colorscheme "catppuccin"
       end
     },
