6d540eae9497f9c7fee63925c82b94a6637fc462
Timur <tmogus@MacBook-Pro-Timur.local>
2026-01-26 21:44:00 +0300

    fixed ai slop

--- lua/config/plugins/lsp.lua
+++ lua/config/plugins/lsp.lua
@@ -11,37 +11,46 @@ return {
     "neovim/nvim-lspconfig",
     dependencies = { "b0o/schemastore.nvim" },
     config = function()
-      vim.lsp.config('lua_ls', {
-        cmd = { 'lua-language-server' },
-        filetypes = { 'lua' },
-        root_markers = { '.luarc.json', '.luarc.jsonc', '.luacheckrc', '.stylua.toml', 'stylua.toml', 'selene.toml', 'selene.yml', '.git' },
+      vim.lsp.config("lua_ls", {
+        cmd = { "lua-language-server" },
+        filetypes = { "lua" },
+        root_markers = {
+          ".luarc.json",
+          ".luarc.jsonc",
+          ".luacheckrc",
+          ".stylua.toml",
+          "stylua.toml",
+          "selene.toml",
+          "selene.yml",
+          ".git",
+        },
         single_file_support = true,
       })
 
-      vim.lsp.config('pyright', {
+      vim.lsp.config("pyright", {
         cmd = { "pyright-langserver", "--stdio" },
-        filetypes = { 'python' },
-        root_markers = { '.git' },
+        filetypes = { "python" },
+        root_markers = { ".git" },
         single_file_support = true,
         settings = {
           python = {
             analysis = {
               autoSearchPaths = true,
               diagnosticMode = "openFilesOnly",
-              useLibraryCodeForTypes = true
-            }
-          }
-        }
+              useLibraryCodeForTypes = true,
+            },
+          },
+        },
       })
 
-      vim.lsp.config('gopls', {
+      vim.lsp.config("gopls", {
         cmd = { "gopls" },
-        filetypes = { 'go', 'gomod', 'gowork', 'gotmpl' },
-        root_markers = { 'go.work', 'go.mod', '.git' },
+        filetypes = { "go", "gomod", "gowork", "gotmpl" },
+        root_markers = { "go.work", "go.mod", ".git" },
         single_file_support = true,
         settings = {
           gopls = {
-            buildFlags = { '-tags=integration' },
+            buildFlags = { "-tags=integration" },
             completeUnimported = true,
             usePlaceholders = true,
             analyses = {
@@ -54,27 +63,25 @@ return {
         },
       })
 
-      vim.lsp.config('rust_analyzer', {
-        cmd = { 'rust-analyzer' },
-        filetypes = { 'rust' },
-        root_markers = { 'Cargo.toml', '.git' },
+      vim.lsp.config("rust_analyzer", {
+        cmd = { "rust-analyzer" },
+        filetypes = { "rust" },
+        root_markers = { "Cargo.toml", ".git" },
         single_file_support = true,
         settings = {
-          ['rust-analyzer'] = {
+          ["rust-analyzer"] = {
             cargo = {
               allFeatures = true,
             },
-            checkOnSave = {
-              command = 'clippy',
-            },
+            checkOnSave = true,
           },
         },
       })
 
-      vim.lsp.config('briefls', {
-        cmd = { 'briefls' },
-        filetypes = { 'brief' },
-        root_markers = { '.git' },
+      vim.lsp.config("briefls", {
+        cmd = { "briefls" },
+        filetypes = { "brief" },
+        root_markers = { ".git" },
         single_file_support = true,
         capabilities = {
           workspace = {
@@ -88,55 +95,56 @@ return {
         },
       })
 
-      vim.lsp.config('jsonls', {
-        cmd = { 'vscode-json-language-server', '--stdio' },
-        filetypes = { 'json', 'jsonc' },
-        root_markers = { '.git' },
+      vim.lsp.config("jsonls", {
+        cmd = { "vscode-json-language-server", "--stdio" },
+        filetypes = { "json", "jsonc" },
+        root_markers = { ".git" },
         single_file_support = true,
         settings = {
           json = {
-            schemas = require('schemastore').json.schemas(),
+            schemas = require("schemastore").json.schemas(),
             validate = { enable = true },
           },
         },
       })
 
-      vim.lsp.enable({ 'lua_ls', 'pyright', 'gopls', 'rust_analyzer', 'briefls', 'jsonls' })
+      vim.lsp.enable({ "lua_ls", "pyright", "gopls", "rust_analyzer", "briefls", "jsonls" })
 
-      vim.api.nvim_create_autocmd('LspAttach', {
+      vim.api.nvim_create_autocmd("LspAttach", {
         callback = function(args)
           -- keymaps
           local map = function(keys, func, desc)
-            vim.keymap.set('n', keys, func, { buffer = args.buf, desc = 'LSP: ' .. desc })
+            vim.keymap.set("n", keys, func, { buffer = args.buf, desc = "LSP: " .. desc })
           end
 
-          map('gd', require('telescope.builtin').lsp_definitions, '[G]oto [D]efinition')
+          map("gd", require("telescope.builtin").lsp_definitions, "[G]oto [D]efinition")
 
-          map('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
+          map("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
 
-          map('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
+          map("gI", require("telescope.builtin").lsp_implementations, "[G]oto [I]mplementation")
 
-          map('<leader>D', require('telescope.builtin').lsp_type_definitions, 'Type [D]efinition')
+          map("<leader>D", require("telescope.builtin").lsp_type_definitions, "Type [D]efinition")
 
-          map('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
+          map("<leader>ds", require("telescope.builtin").lsp_document_symbols, "[D]ocument [S]ymbols")
 
-          map('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
+          map("<leader>ws", require("telescope.builtin").lsp_dynamic_workspace_symbols, "[W]orkspace [S]ymbols")
 
-          map('<leader>rn', vim.lsp.buf.rename, '[R]e[n]ame')
+          map("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
 
-          map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
+          map("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
 
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
+          map("K", vim.lsp.buf.hover, "Hover Documentation")
 
-          map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
-
+          map("gD", vim.lsp.buf.declaration, "[G]oto [D]eclaration")
 
           local client = vim.lsp.get_client_by_id(args.data.client_id)
-          if not client then return end
+          if not client then
+            return
+          end
 
-          if client:supports_method('textDocument/formatting') then
+          if client:supports_method("textDocument/formatting") then
             -- Format the current buffer on save
-            vim.api.nvim_create_autocmd('BufWritePre', {
+            vim.api.nvim_create_autocmd("BufWritePre", {
               buffer = args.buf,
               callback = function()
                 vim.lsp.buf.format({ bufnr = args.buf, id = client.id })
@@ -146,5 +154,5 @@ return {
         end,
       })
     end,
-  }
+  },
 }
